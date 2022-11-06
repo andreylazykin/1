17efe551f1a94016ddb75edd3a218fbd8bce0eae
Андрей Лазыкин <95586771+andreylazykin@users.noreply.github.com>
2022-11-06 13:15:29 +0400

    Add files via upload

---
+++ 1-7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("a=");
+            int a = byte.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = byte.Parse(Console.ReadLine());
+            int c = a & b;
+            Console.WriteLine(Convert.ToString(c, toBase: 2));
+            int d = a ^ b;
+            Console.WriteLine(Convert.ToString(d, toBase: 2));
+            int e = a | b;
+            Console.WriteLine(Convert.ToString(e, toBase: 2));
+        }
+    }
+}
+
