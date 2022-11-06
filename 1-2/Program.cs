8e73436b1545e5cfa770bd5ad8f64a11e928263b
Андрей Лазыкин <95586771+andreylazykin@users.noreply.github.com>
2022-11-06 13:10:47 +0400

    Add files via upload

---
+++ 1-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("N=");
+            int N = int.Parse(Console.ReadLine());
+            string a = "*";
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(a);
+                a += "*";
+            }
+            return;
+        }
+    }
+}
+
---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int c;
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            if ((a <= 0) || (b <= 0))
+                Console.WriteLine ("Oshibka");
+            if ((a > 0) && (b > 0))
+                Console.WriteLine(c = a * b);
+
+        }
+    }
+}
